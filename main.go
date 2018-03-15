1f74bc86bb5134d749bd9a38961f0d952e8bd46c
Chavez <matthew@el-chavez.me>
2018-03-14 17:51:50 -0700

    fix: Exit on empty required flags

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/mtchavez/aws-mfa-sts/cli"
 )
@@ -12,6 +14,16 @@ func main() {
 	region := flag.String("region", "us-east-1", "AWS Region")
 	deviceArn := flag.String("device-arn", "", "MFA Serial Number")
 	flag.Parse()
+	if *token == "" {
+		fmt.Println("Token required")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *deviceArn == "" {
+		fmt.Println("MFA Device ARN required. Please go to your IAM user and copy.")
+		flag.Usage()
+		os.Exit(1)
+	}
 	app := cli.NewApp(*profile, *token, *region, *deviceArn)
 	app.SetupUser()
 }
